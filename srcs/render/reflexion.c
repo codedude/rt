56e0c1df0ca6a47591b995ea561fce5c7b34d39a
hcaillau <hcaillau@student.42.fr>
2018-04-26 19:22:29 +0200

    reflexion

--- srcs/render/reflexion.c
+++ srcs/render/reflexion.c
@@ -12,8 +12,7 @@ t_ray				reflected_ray(t_inter inter, t_vec ray)
 	t_ray	ret;
 
 	ret.origin = inter.point;
-	ret.dir = inter.normal * (2 * vec_dot(inter.normal, ray));
-	ret.dir = ret.dir - ray;
+	ret.dir = inter.normal * (2.0 * vec_dot(inter.normal, ray)) - ray;
 	return (ret);
 }
 
@@ -25,7 +24,7 @@ t_vec				reflexion(t_rt *rt, t_hit *hit, int depth, t_vec color)
 	reflected_color = VEC_ZERO;
 	if (hit->inter.obj->reflexion > 0 && depth > 0)
 	{
-		reflect.ray = reflected_ray(hit->inter, (-1) * hit->ray.dir);
+		reflect.ray = reflected_ray(hit->inter, -1.0 * hit->ray.dir);
 		reflect.ray.refraction = hit->ray.refraction;
 		reflected_color = compute_color(rt, &reflect, depth - 1);
 	}
