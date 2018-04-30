021780c8fe431ebd70ffbf95da612bdbc9e22c1c
Valentin PARIS <vparis@student.42.fr>
2018-04-30 16:36:27 +0200

    up

--- includes/sdl_m.h
+++ includes/sdl_m.h
@@ -6,7 +6,7 @@
 /*   By: vparis <vparis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 14:24:09 by vparis            #+#    #+#             */
-/*   Updated: 2018/04/30 16:23:50 by vparis           ###   ########.fr       */
+/*   Updated: 2018/04/30 16:31:12 by vparis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include <stdlib.h>
 # include "SDL.h"
-//# include "/usr/local/include/SDL2/SDL.h"
 # include "types.h"
 
 typedef struct		s_sdl {
